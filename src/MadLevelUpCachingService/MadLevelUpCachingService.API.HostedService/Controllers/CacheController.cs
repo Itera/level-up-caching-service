d4c44ae8f889673b664f18936a277b7e344bbccc
Andreas Johnsen Lind <andreas.johnsen.lind@itera.com>
2021-10-21 13:52:15 +0200

    Fixing .gitignore issues

---
+++ src/MadLevelUpCachingService/MadLevelUpCachingService.API.HostedService/Controllers/CacheController.cs
@@ -0,0 +1,26 @@
+﻿using MadLevelUpCachingService.API.ScheduledService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadLevelUpCachingService.API.HostedService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CacheController : ControllerBase
+    {
+        // 
+        // GET: /Events/
+        [HttpGet()]
+        public string Index()
+        {
+            return Worker.getCache();
+        }
+
+        // 
+        // GET: /Events/
+        [HttpGet("{id}")]
+        public ActionResult Index(string id)
+        {
+            return this.Content(Worker.getCacheEntry(id), "application/json");
+        }
+    }
+}
---
+++ src/MadLevelUpCachingService/MadLevelUpCachingService/Startup.cs
@@ -0,0 +1,98 @@
+using MadLevelUpCachingService.API.HostedService;
+using MadLevelUpCachingService.API.ScheduledService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+
+namespace MadLevelUpCachingService
+{
+    public class Startup
+    {
+        public IHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            /*
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(this.Configuration, "AzureAd")
+                .EnableTokenAcquisitionToCallDownstreamApi()
+                //.AddMicrosoftGraph(this.Configuration.GetSection("GraphApi"))
+                .AddInMemoryTokenCaches();
+            */
+
+            services.AddLogging();
+
+            services.AddCors(options =>
+            {
+                var origins = Environment.IsDevelopment() ?
+                    new[] { "http://localhost:5003", "https://localhost:5004" } :
+                    new[] { "https://learning-cache.mad.itera.no" };
+
+                options.AddDefaultPolicy(
+                    builder => builder
+                        .WithOrigins(origins)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+            });
+
+            services.AddHostedService<Worker>();
+
+            services.AddMvc();
+            services.AddControllers();
+            services.AddSwaggerGen(static c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MadLevelUpCachingService", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!string.IsNullOrWhiteSpace(this.Configuration["PathBase"]))
+                app.UsePathBase(this.Configuration["PathBase"]);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(static c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MadLearning.API v1"));
+            }
+            else
+            {
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.All,
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseCors();
+            /*
+            app.UseAuthentication();
+            app.UseAuthorization();
+            */
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}");
+            });
+        }
+    }
+}
